5f92bae1440b2b22bd511a7136693a75dfb7878d
Amir-Toolabi-pro <amirooogamer81@gmail.com>
2023-03-02 18:32:31 +0330

    Navbar Designing

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import FormContact from "./contact/FormContact";
+
+import "../styles/navbar.css"
+
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div className="container">
+          <div className="nav_title">
+            <p>
+              <i className="fas fa-id-badge" />
+              وب اپلیکیشن مدیریت {"  "}
+              <span>مخاطبین</span>
+            </p>
+            </div>
+            <div className="form_contact_holder">
+              <FormContact/>
+            </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/components/contact/FormContact.jsx
@@ -0,0 +1,18 @@
+
+//style
+import "../../styles/formcontact.css"
+
+const FormContact = () => {
+  return (
+    <>
+      <form dir="ltr" >
+        <span className="input-group-text " id="basic-addon1" >
+          <i className="fas fa-search" />
+        </span>
+        <input className="form-control" type="text" dir="rtl" placeholder="جستوجو مخاطب" aria-label="search" aria-describedby="basic-addon1" />
+      </form>
+    </>
+  );
+}
+
+export default FormContact;
