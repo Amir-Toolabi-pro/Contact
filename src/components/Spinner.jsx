06a613ee5f53add6e4d45438721d5fefcdd7832d
Amir-Toolabi-pro <amirooogamer81@gmail.com>
2023-03-03 08:11:40 +0330

    add spinner loadin to project

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+
+
+const Spinner = () => {
+  return (
+    <>
+      <div style={{ display: "flex", width: "100%", height: "80vh", justifyContent: "center", alignItems: "center" }}>
+        <img src={require("../images/Spinner.gif")} style={{ width: "150px" }} />
+      </div>
+    </>
+  );
+}
+
+export default Spinner;
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,22 +6,32 @@ import CardMap from "./cards/CardMap";
 import "../../styles/contact.css"
 import { useState } from "react";
 import NotFound from "./cards/NotFound";
+import Spinner from "../Spinner";
 
 const Contact = () => {
 
-  const [contacts , setContacts] = useState([])
+  const [contacts, setContacts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   return (
     <>
-      <Navbar/>
-      <AddContact/>
-      <section className="contact_body" >
-        {contacts.length > 0 ?
-          <CardMap/> :
-          <NotFound/>
-        }
-        
-      </section>
+      <Navbar />
+
+      {loading ?
+        undefined :
+        <AddContact />
+      }
+
+      {loading ?
+        <Spinner /> :
+        <section className="contact_body" >
+          {contacts.length > 0 ?
+            <CardMap /> :
+            <NotFound />
+          }
+        </section>
+      }
+
     </>
   );
 }
